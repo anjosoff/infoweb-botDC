bb3b1808a3b38ae1b51dd68dd0a112a6f5282a56
Guilherme Oliveira dos Anjos <40221817+anjosoff@users.noreply.github.com>
2021-12-16 02:22:51 +0000

    avisar was redone

--- commands/avisar.js
+++ commands/avisar.js
@@ -1,14 +1,17 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
+  const aviso = args.join(' ');
+  message.delete().catch(O_o => {});
   if(!message.member.hasPermission(["MANAGE_GUILD","MANAGE_ROLES","ADMINISTRATOR"])){
-    return message.channel.send({embed: {color:'RED',author:{name:`Vish, não consegui!`}, description:"Você não pode criar um aviso, bobinho",footer:{text:'Duvidas? zap!help | developed by @anjosoff'}}})
-  }else{
-    const sayMessage = args.join(' ');
-    message.delete().catch(O_o => {});
-    message.channel.send({embed:{color:'RED',description:`**AVISO DE ${message.author.username}**\n ${sayMessage}`,timestamp: new Date(),footer:{text:`Duvidas? zap!help | Aviso criado via comando por: ${message.author.username}`}}});
-    console.log(`Comamand required for ${message.author.username}`);
-
+    return message.channel.send({embed: {color:'YELLOW',author:{name:`Vish, não consegui!`}, description:"Você não pode criar um aviso, bobinho",footer:{text: `Duvidas? zap!help | developed by @anjosoff | Tentativa de criação de aviso via comando por: ${message.author.username} `}}}).then(msg => {
+    msg.delete({ timeout: 10000})});
   }
+    if(aviso==''){
+      return message.channel.send({embed: {color:'YELLOW',author:{name:`Vish, não consegui!`}, description:"Você não colocou nada no aviso, bobinho",footer:{text: `Duvidas? zap!help | developed by @anjosoff | Tentativa de criação de aviso via comando por: ${message.author.username} `}}}).then(msg => {msg.delete({ timeout: 10000})});
+    }else{
+      message.channel.send({embed:{color:'RED',description:`**AVISO DE ${message.author.username}**\n ${aviso}`,timestamp: new Date(),footer:{text:`Duvidas? zap!help | Aviso criado via comando por: ${message.author.username}`}}});
+    console.log(`Comamand required for ${message.author.username}`);
+    }
   
 };
